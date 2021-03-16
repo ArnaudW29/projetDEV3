bb1847fd2bee56d669e411c56b44582cbf1f7f67
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-16 09:10:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
   
 const express = require("express")
-const fs = require("fs")
-
 const app = express()
 
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 5000)

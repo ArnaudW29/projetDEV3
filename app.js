05457b822b3ea45f78ca168d5322b90239e1b7c6
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-16 09:39:41 +0100

    heroku deploy

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`projetDev3 on port ${port}!`));

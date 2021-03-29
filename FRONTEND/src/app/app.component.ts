a0a9cda1c0948a408aa12e6c60b50edd766b4925
Nicolas Daxhelet <nicolas.daxhelet@outlook.be>
2021-03-29 18:58:44 +0200

    fin d'adaptation de la maquette à angular & ébauche de API

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -7,62 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FRONTEND';
-}
 
-//Déclaration des variables
-let game_1_description = "<p class='horizontal_center'>ceci est la description du premier jeu</p>"
-let game_2_description = "<p class='horizontal_center'>ceci est la description du deuxieme jeu</p>"
-let game_3_description = "<p class='horizontal_center'>ceci est la description du troisieme jeu</p>"
-let game_4_description = "<p class='horizontal_center'>ceci est la description du quatrieme jeu</p>"
+  //Déclaration des fonctions
+  get_description(game_number: string) {
+    let image_1 = document.getElementById("game_1_image")!
+    let image_2 = document.getElementById("game_2_image")!
+    let image_3 = document.getElementById("game_3_image")!
+    let image_4 = document.getElementById("game_4_image")!
+    switch(game_number) {
+      case "game_1":
+        image_1.style.border = "1px solid #77216f"
+        image_1.style.borderRadius = "10px"
+        image_2.style.border = "none"
+        image_3.style.border = "none"
+        image_4.style.border = "none"
+        this.make_description(game_number)
+        break
+      case "game_2":
+        image_2.style.border = "1px solid #77216f"
+        image_2.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_3.style.border = "none"
+        image_4.style.border = "none"
+        this.make_description(game_number)
+        break
+      case "game_3":
+        image_3.style.border = "1px solid #77216f"
+        image_3.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_2.style.border = "none"
+        image_4.style.border = "none"
+        this.make_description(game_number)
+        break
+      case "game_4":
+        image_4.style.border = "1px solid #77216f"
+        image_4.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_2.style.border = "none"
+        image_3.style.border = "none"
+        this.make_description(game_number)
+        break	
+    }
+  }
 
-let game_1_leaderboard = {"pseudo1": "XXXX", "pseudo2": "XXXY", "pseudo3": "XYXY", "pseudo4": "YYYX", "pseudo5": "YYYY"}
-let game_2_leaderboard = {"pseudo4": "XYYX", "pseudo3": "XXY", "pseudo1": "XYXY"}
-let game_3_leaderboard = {"pseudo2": "X"}
-let game_4_leaderboard = {"pseudo6": "XXXX", "pseudo7": "XXXY", "pseudo10": "YYYY"}
-
-//Déclaration des fonctions
-function initialiserPage() {
-
-}
-
-function get_description(game_number: string) {
-  let description = document.getElementById("active_game_description")!
-  let image_1 = document.getElementById("game_1_image")!
-  let image_2 = document.getElementById("game_2_image")!
-  let image_3 = document.getElementById("game_3_image")!
-  let image_4 = document.getElementById("game_4_image")!
-  switch(game_number) {
-    case "game_1":
-      description.innerHTML = game_1_description
-      image_1.style.border = "1px solid #77216f"
-      image_1.style.borderRadius = "10px"
-      image_2.style.border = "none"
-      image_3.style.border = "none"
-      image_4.style.border = "none"
-      break
-    case "game_2":
-      description.innerHTML = game_2_description
-      image_2.style.border = "1px solid #77216f"
-      image_2.style.borderRadius = "10px"
-      image_1.style.border = "none"
-      image_3.style.border = "none"
-      image_4.style.border = "none"
-      break
-    case "game_3":
-      description.innerHTML = game_3_description
-      image_3.style.border = "1px solid #77216f"
-      image_3.style.borderRadius = "10px"
-      image_1.style.border = "none"
-      image_2.style.border = "none"
-      image_4.style.border = "none"
-      break
-    case "game_4":
-      description.innerHTML = game_4_description
-      image_4.style.border = "1px solid #77216f"
-      image_4.style.borderRadius = "10px"
-      image_1.style.border = "none"
-      image_2.style.border = "none"
-      image_3.style.border = "none"
-      break	
+  make_description(game_number: string) {
+    let description = document.getElementById("active_game_description")!
+    let xhr = new XMLHttpRequest();
+    let leaderboard = {};
+    let str = '<tr style="border:1px solid #fff;color:#e95420"><th colspan="2">pseudo</th><th colspan="2">nb points</th></tr>';
+    xhr.open('get','http://localhost:5000/description/' + game_number);
+    xhr.onload = function(){
+      description.innerHTML = xhr.responseText
+    }
+    xhr.send();
   }
 }
--- FRONTEND/src/app/side/side.component.ts
+++ FRONTEND/src/app/side/side.component.ts
@@ -9,7 +9,66 @@ export class SideComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  get_leaderboard(game_number: string) {
+    let table = document.getElementById("scores")!
+    let image_1 = document.getElementById("game_logo_1")!
+    let image_2 = document.getElementById("game_logo_2")!
+    let image_3 = document.getElementById("game_logo_3")!
+    let image_4 = document.getElementById("game_logo_4")!
+    switch(game_number) {
+      case "game_1":
+        image_1.style.border = "1px solid #e95420"
+        image_1.style.borderRadius = "10px"
+        image_2.style.border = "none"
+        image_3.style.border = "none"
+        image_4.style.border = "none"
+        this.make_leaderboard(game_number)
+        break
+      case "game_2":
+        image_2.style.border = "1px solid #e95420"
+        image_2.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_3.style.border = "none"
+        image_4.style.border = "none"
+        this.make_leaderboard(game_number)
+        break
+      case "game_3":
+        image_3.style.border = "1px solid #e95420"
+        image_3.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_2.style.border = "none"
+        image_4.style.border = "none"
+        this.make_leaderboard(game_number)
+        break
+      case "game_4":
+        image_4.style.border = "1px solid #e95420"
+        image_4.style.borderRadius = "10px"
+        image_1.style.border = "none"
+        image_2.style.border = "none"
+        image_3.style.border = "none"
+        this.make_leaderboard(game_number)
+        break
+    }
+  }
+  
+  make_leaderboard(game_number: string) {
+    let xhr = new XMLHttpRequest();
+    let leaderboard = {};
+    let str = '<tr style="border:1px solid #fff;color:#e95420"><th colspan="2">pseudo</th><th colspan="2">nb points</th></tr>';
+    xhr.open('get','http://localhost:5000/leaderboard/' + game_number);
+    xhr.onload = function(){
+      leaderboard = JSON.parse(xhr.responseText);
+      let pseudos = Object.keys(leaderboard)
+      let scores = Object.values(leaderboard)
+      for(let i = 0; i < pseudos.length; i++) {
+        str += '<tr><td style="border-left:1px solid #fff;border-right:1px solid #fff;color:#fff" colspan="2">' + pseudos[i]
+        str += '</td><td style="border-left:1px solid #fff;border-right:1px solid #fff;color:#fff" colspan="2">' + scores[i] + '</td></tr>'
+      }
+      let table = document.getElementById("scores")!;
+      table.innerHTML = str;
+    }
+    xhr.send();
+  }
 }

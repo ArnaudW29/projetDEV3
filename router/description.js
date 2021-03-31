c57a09afd993b23ca67bba602f882d07de5fbf2a
Nicolas Daxhelet <nicolas.daxhelet@outlook.be>
2021-03-31 14:34:52 +0200

    [nettoyage] API
    suppression des fichiers restants du transfert de l'api de /BACKEND/script-api-rest à /
    
    nettoyage du code des routes GET pour les descriptions des jeux et les leaderboard. 
    
    simulation d'une "base de donnée" avec une chaîne de caractères JSON

--- router/description.js
+++ router/description.js
@@ -22,37 +22,57 @@ router.get('/', function (req, res) {
 //     .put(contactController.update)
 //     .delete(contactController.delete);
 
+// variables
+const json_data = '{' +
+    '"games":[' +
+        '{' +
+            '"name":"game_1",' +
+            '"description":"<p style=\'border:1px solid #77216f;border-radius:5px;padding:10px\' class=\'horizontal_center\'>ceci est la description du premier jeu</p>",' + 
+            '"hardcoded_leaderboard": {"pseudo1": "XXXX", "pseudo2": "XXXY", "pseudo3": "XYXY", "pseudo4": "YYYX", "pseudo5": "YYYY"}' +
+        '},' +
+        '{' +
+            '"name":"game_2",' +
+            '"description":"<p style=\'border:1px solid #77216f;border-radius:5px;padding:10px\' class=\'horizontal_center\'>ceci est la description du deuxieme jeu</p>",' +
+            '"hardcoded_leaderboard": {"pseudo4": "XYYX", "pseudo3": "XXY", "pseudo1": "XYXY"}' +
+        '},' +
+        '{' +
+            '"name":"game_3",' +
+            '"description":"<p style=\'border:1px solid #77216f;border-radius:5px;padding:10px\' class=\'horizontal_center\'>ceci est la description du troisieme jeu</p>",' +
+            '"hardcoded_leaderboard": {"pseudo2": "X"}' +
+        '},' +
+        '{' +
+            '"name":"game_4",' + 
+            '"description":"<p style=\'border:1px solid #77216f;border-radius:5px;padding:10px\' class=\'horizontal_center\'>ceci est la description du quatrieme jeu</p>",' +
+            '"hardcoded_leaderboard": {"pseudo6": "XXXX", "pseudo7": "XXXY", "pseudo10": "YYYY"}' +
+        '}' +
+    ']' +
+'}'
+
+let data = JSON.parse(json_data);
+
 // get game1 description
-let game_1_description = "<p style='border:1px solid #77216f;border-radius:5px;padding:10px' class='horizontal_center'>ceci est la description du premier jeu</p>"
-router.get('/game_1', (req, res) => res.send(game_1_description));
+router.get('/game_1', (req, res) => res.send(data.games[0].description));
 
 // get game2 description
-let game_2_description = "<p style='border:1px solid #77216f;border-radius:5px;padding:10px' class='horizontal_center'>ceci est la description du deuxieme jeu</p>"
-router.get('/game_2', (req, res) => res.send(game_2_description));
+router.get('/game_2', (req, res) => res.send(data.games[1].description));
 
 // get game3 description
-let game_3_description = "<p style='border:1px solid #77216f;border-radius:5px;padding:10px' class='horizontal_center'>ceci est la description du troisieme jeu</p>"
-router.get('/game_3', (req, res) => res.send(game_3_description));
+router.get('/game_3', (req, res) => res.send(data.games[2].description));
 
 // get game4 description
-let game_4_description = "<p style='border:1px solid #77216f;border-radius:5px;padding:10px' class='horizontal_center'>ceci est la description du quatrieme jeu</p>"
-router.get('/game_4', (req, res) => res.send(game_4_description));
+router.get('/game_4', (req, res) => res.send(data.games[3].description));
 
 // get game1 leaderbord
-let game_1_leaderboard = {"pseudo1": "XXXX", "pseudo2": "XXXY", "pseudo3": "XYXY", "pseudo4": "YYYX", "pseudo5": "YYYY"}
-router.get('/leaderboard/game_1', (req, res) => res.send(game_1_leaderboard));
+router.get('/leaderboard/game_1', (req, res) => res.send(data.games[0].hardcoded_leaderboard));
 
 // get game2 leaderbord
-let game_2_leaderboard = {"pseudo4": "XYYX", "pseudo3": "XXY", "pseudo1": "XYXY"}
-router.get('/leaderboard/game_2', (req, res) => res.send(game_2_leaderboard));
+router.get('/leaderboard/game_2', (req, res) => res.send(data.games[1].hardcoded_leaderboard));
 
 // get game3 leaderbord
-let game_3_leaderboard = {"pseudo2": "X"}
-router.get('/leaderboard/game_3', (req, res) => res.send(game_3_leaderboard));
+router.get('/leaderboard/game_3', (req, res) => res.send(data.games[2].hardcoded_leaderboard));
 
 // get game4 leaderbord
-let game_4_leaderboard = {"pseudo6": "XXXX", "pseudo7": "XXXY", "pseudo10": "YYYY"}
-router.get('/leaderboard/game_4', (req, res) => res.send(game_4_leaderboard));
+router.get('/leaderboard/game_4', (req, res) => res.send(data.games[3].hardcoded_leaderboard));
 
 // test post request
 // app.post("/add", (req, res) => {

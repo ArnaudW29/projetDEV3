054f76be1bd8ee9644e2cd08b2e99133031a3882
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2021-04-18 17:34:06 +0200

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String
+    username: String,
+    password: String
 })
 
 module.exports = mongoose.model('user', userSchema, 'users')

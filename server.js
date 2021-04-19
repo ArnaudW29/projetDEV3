8366ba15bdce378d5e607d54639d40ff52de7ece
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-04-19 11:33:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // express & path & cors import
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
 let cors = require('cors');
 
 // express app creation

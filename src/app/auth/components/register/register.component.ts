085d1ec5815a20cd3b6e2b6c498638585c67e31b
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-04-20 17:02:15 +0200

    Register form

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor() { }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  ngOnInit(): void {
+  }
+
+}
